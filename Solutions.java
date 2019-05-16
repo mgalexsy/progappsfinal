93e67992ff3f75808857c8fcfd191c403fdf4731
Alejo <34014413+mgalexsy@users.noreply.github.com>
2019-05-16 16:20:40 +0800

    Inserted 1077A Java code

--- Solutions.java
+++ Solutions.java
@@ -183,4 +183,22 @@ public class CompiledCodes{
     return result;
   }
 
+public class 1077A {
+	public static void main(String[] args) {
+		Scanner scanny = new Scanner(System.in);
+		int x = scanny.nextInt();
+		for(int i = 0; i < x; i++) {
+			long r = scanny.nextInt();
+			long l = scanny.nextInt();
+			long y = scanny.nextInt();
+			if(y%2==0) System.out.println((r-l)*(y/2));
+			else System.out.println((r-l)*((y-1)/2)+r);
+		}
+	}
+}
+
+
+
+
+
 }
