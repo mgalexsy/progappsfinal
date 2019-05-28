b74fae819a8536ab4114979dcd04bca5d17e2023
Alejo <34014413+mgalexsy@users.noreply.github.com>
2019-05-28 16:55:45 +0800

    Edited Indentation

--- 1077A-Frog-Jumping.java
+++ 1077A-Frog-Jumping.java
@@ -1,15 +1,15 @@
 public int jumpingFrog(int r, int l, int y)  {
-		Scanner scanner = new Scanner(System.in);
-		int x = scanner.nextInt();
-		for(int i = 0; i < x; i++) {
-		   r = scanner.nextInt();
-			l = scanner.nextInt();
-			y = scanner.nextInt();
-		}
-      if(y%2==0) {
-         return ((r-l)*(y/2));
-	   } else { 
-         return ((r-l)*((y-1)/2)+r);
-      }
-
+	Scanner scanner = new Scanner(System.in);
+	int x = scanner.nextInt();
+	for(int i = 0; i < x; i++) {
+	    r = scanner.nextInt();
+	    l = scanner.nextInt();
+	    y = scanner.nextInt();
 	}
+        if(y%2==0) {
+           return ((r-l)*(y/2));
+	} else { 
+           return ((r-l)*((y-1)/2)+r);
+        }
+
+}
