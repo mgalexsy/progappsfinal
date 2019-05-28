07d625a50ca0e70b44bc78ef5b3a0f1cf506490f
Alejo <34014413+mgalexsy@users.noreply.github.com>
2019-05-28 15:52:06 +0800

    Updated to Method

--- 1077A-Frog-Jumping.java
+++ 1077A-Frog-Jumping.java
@@ -1,15 +1,15 @@
-import java.util.*;
-
-public class 1077A {
-	public static void main(String[] args) {
+public int jumpingFrog(int r, int l, int y)  {
 		Scanner scanny = new Scanner(System.in);
 		int x = scanny.nextInt();
 		for(int i = 0; i < x; i++) {
-			long r = scanny.nextInt();
-			long l = scanny.nextInt();
-			long y = scanny.nextInt();
-			if(y%2==0) System.out.println((r-l)*(y/2));
-			else System.out.println((r-l)*((y-1)/2)+r);
+		   r = scanny.nextInt();
+			l = scanny.nextInt();
+			y = scanny.nextInt();
 		}
+      if(y%2==0) {
+         return ((r-l)*(y/2));
+	   } else { 
+         return ((r-l)*((y-1)/2)+r);
+      }
+
 	}
-}
