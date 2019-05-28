6727797fcccfd727758216becb15a33644d82e6c
2156667 <50197948+2156667@users.noreply.github.com>
2019-05-28 17:17:37 +0800

    Added my source code

--- Solutions.java
+++ Solutions.java
@@ -38,3 +38,13 @@ public class Solutions{
 		}
       return (r+" "+2*r);
 	}
+
+//Fider
+public int rollingDice(int n, int goal) {
+        Random s = null;
+        n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            goal = s.nextInt();
+        }
+        return goal / 2;
+    }
