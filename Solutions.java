363c3e3e3b4cabdb97cc3b5ddaa224209601a964
Alejo <34014413+mgalexsy@users.noreply.github.com>
2019-05-29 12:33:25 +0800

    Edited indentations

--- Solutions.java
+++ Solutions.java
@@ -5,17 +5,17 @@ public class Solutions {
 		Scanner scanner = new Scanner(System.in);
 		int x = scanner.nextInt();
 		for(int i = 0; i < x; i++) {
-	    	r = scanner.nextInt();
-	    	l = scanner.nextInt();
-	    	y = scanner.nextInt();
-	}
+	    	    r = scanner.nextInt();
+	    	    l = scanner.nextInt();
+	    	    y = scanner.nextInt();
+	        }
         	if (y%2==0) {
                    return ((r-l)*(y/2));
 		} else { 
            	   return ((r-l)*((y-1)/2)+r);
-        }
+                }
 
-}
+          }
 
 	//Siababa
     	public int get (int firstNum, int secondNum){
