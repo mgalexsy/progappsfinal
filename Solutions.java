ad285d430d4c0fd242dc651a8ec2fdcfc3bab126
djigz <33065540+djigz@users.noreply.github.com>
2019-05-29 12:02:07 +0800

    converted code into method

--- Solutions.java
+++ Solutions.java
@@ -73,6 +73,30 @@ public class Solutions {
       		}
       		return result;
       }
+
+	//Cayadi
+	public int getThanosNim(int n, int a, int min){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int min = 51;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            min = Math.min(min, a[i]);
+        }
+        int minCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == min) {
+                minCount++;
+            }
+        }
+        if (minCount <= n / 2) {
+            System.out.println("Alice");
+        } else {
+            System.out.println("Bob");
+        }
+
+    }
 }
 	
 	
