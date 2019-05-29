26d759854db70a8a8ba8c673f8e7c2ded6895325
djigz <33065540+djigz@users.noreply.github.com>
2019-05-29 12:04:20 +0800

    ThanosNim

---
+++ 1162E.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ThanosNim {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int min = 51;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            min = Math.min(min, a[i]);
+        }
+        int minCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == min) {
+                minCount++;
+            }
+        }
+        if (minCount <= n / 2) {
+            System.out.println("Alice");
+        } else {
+            System.out.println("Bob");
+        }
+
+    }
+}
