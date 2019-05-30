6d55267d3f1742c46eb58b6d143e2af4f34f2e00
AceSoriano <37828272+AceSoriano@users.noreply.github.com>
2019-05-30 17:55:42 +0800

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -139,5 +139,25 @@ public int rollingDice() {
 		}
 		return (secondNum+" "+2*secondNum);
 	}
+	public int getRemainder (){
+        	Scanner sc=new Scanner(System.in);
+        	int firstInput=sc.nextInt();
+        	int secondInput=sc.nextInt();
+        	int thirdInput=sc.nextInt();
+        	String oneOrZero =sc.next();
+        	int count=0;
+        	for(int i=firstInput-secondInput;i<firstInput;i++) {
+            		if(i==firstInput-thirdInput-1) {
+                		if(oneOrZero.charAt(i)!='1') {
+                    			count++;
+                		}
+            		}
+            		else if(oneOrZero.charAt(i)=='1') {
+                		count++;
+            		}
+        	}
+
+        	return count;
+    	}
 
 }
