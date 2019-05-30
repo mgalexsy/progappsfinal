273a0ab9f5524b9ecbb566726fe3397a8c9aa7fc
2156667 <50197948+2156667@users.noreply.github.com>
2019-05-30 15:47:27 +0800

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -99,4 +99,13 @@ public class Solutions {
   }
 }
 	
-	
+	//Fider
+
+   public int rollingDice(int n, int goal) {
+        Random s = null;
+        n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            goal = s.nextInt();
+        }
+        return goal / 2;
+    }
