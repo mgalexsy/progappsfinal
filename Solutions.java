cc2a5cd1c80725b46a9d459405b169695deb9161
2156667 <50197948+2156667@users.noreply.github.com>
2019-05-30 16:51:49 +0800

    Edited the indentation

--- Solutions.java
+++ Solutions.java
@@ -101,10 +101,10 @@ public class Solutions {
 	
 	//Fider
 
-   public int rollingDice(int num, int goal) {
-        Random  = null;
-       		 n = s.nextInt();
-       			for (int num = 0; i < n; num++) {
+public int rollingDice(int n, int goal) {
+        Random s = null;
+        n = s.nextInt();
+        for (int i = 0; i < n; i++) {
             goal = s.nextInt();
         }
         return goal / 2;
