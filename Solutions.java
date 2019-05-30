61e4c333d15cc9b65c4bc1c03c38b6fd76eba364
Michael Fider <50197948+2156667@users.noreply.github.com>
2019-05-30 17:50:53 +0800

    changed the variables

--- Solutions.java
+++ Solutions.java
@@ -106,12 +106,12 @@ public class Solutions {
 	//Fider
 
 public int rollingDice() {
-        Random t = null;
-	int n = 0;
+        Random integer = null;
+	int one = 0;
 	int goal = 0;
-        n = s.nextInt();
-        for (int i = 0; i < n; i++) {
-            goal = s.nextInt();
+        n = integer.nextInt();
+        for (int i = 0; i < n; one++) {
+            goal = integer.nextInt();
         }
         return goal / 2;
     }
